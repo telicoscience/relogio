ec31283598d3d5066ad847a763ad8e51c25a4b15
Télico Oliveira <telico@yahoo.com.br>
2022-09-10 18:44:42 -0300

    corrigido o bug dos ponteiros

--- relogio.js
+++ relogio.js
@@ -9,8 +9,8 @@ let hh = day.getHours()*30;
 let mm = day.getMinutes()*deg; 
 let ss = day.getSeconds()*deg; 
 
-hr.style.transform = 'rotateZ(${(hh) + (mm/12)}deg)'
-mn.style.transform = 'rotateZ(${(mm)}deg)'
-sc.style.transform = 'rotateZ(${(ss)}deg)'
+hr.style.transform = `rotateZ(${(hh) + (mm/12)}deg)`
+mn.style.transform = `rotateZ(${(mm)}deg)`
+sc.style.transform = `rotateZ(${(ss)}deg)`
 }
 )
